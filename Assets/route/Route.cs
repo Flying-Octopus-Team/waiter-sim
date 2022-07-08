362e43b56cd096e58670048e4d446edba733a395
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-07-08 23:07:28 +0200

    Add route with main route element moving towards camera with variable speed

---
+++ Assets/route/Route.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace route
+{
+    public class Route : MonoBehaviour
+    {
+        public bool routeRunning;
+
+
+        public float mainElementStartingSpeed = 0f;
+        public float mainElementTargetScale = 2f;
+
+        public RouteElement mainRouteElement;
+        private List<RouteElement> _elements;
+
+        private void Start()
+        {
+            LoadRouteElements();
+        }
+
+        private void Update()
+        {
+            if (routeRunning)
+            {
+                _elements.ForEach(el => el.ScaleAndMoveElement());
+                if (mainRouteElement.GetElementScale() >= mainElementTargetScale)
+                {
+                    routeRunning = false;
+                }
+            }
+        }
+
+        public void StartRoute()
+        {
+            routeRunning = true;
+            mainRouteElement.SetBaseSpeed(mainElementStartingSpeed);
+            LoadRouteElements();
+        }
+
+        private void LoadRouteElements()
+        {
+            //TODO don't load generate thiz
+            _elements = new List<RouteElement>(transform.GetComponentsInChildren<RouteElement>());
+        }
+    }
+}
