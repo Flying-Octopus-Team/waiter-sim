5e21e514d80469d1667623f68a7c7b12cbb486ba
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-07-09 19:50:51 +0200

    Explosions!

---
+++ Assets/GFX/GameOver/ScreenShatterEffect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace GFX.GameOver
+{
+    public class ScreenShatterEffect : MonoBehaviour
+    {
+        private void Start()
+        {
+            var explosionPosition = new Vector3(1000.405f,999.253f,1000.046f);
+            foreach (Transform child in transform)
+            {
+                if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+                {
+                    childRigidbody.AddExplosionForce(5f, explosionPosition, 10f);
+                }
+            }
+        }
+    }
+}
