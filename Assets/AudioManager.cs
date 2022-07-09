d684f3e7b3b56d5163009d31d8b8ddf09479a2c6
Kennedy ツ <wojewoodzki@gmail.com>
2022-07-10 00:15:02 +0200

    Audio Manager, v2

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Level;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
@@ -11,19 +14,30 @@ public class AudioManager : MonoBehaviour
 	[SerializeField, Range(0, 20)] private int intervalMax = 12;
 	
 	[SerializeField] private AudioSource musicSource;
+	[SerializeField] private AudioSource ambienceChatterSource;
 	[SerializeField] private AudioSource sfxSource;
 	
 	[SerializeField] private AudioClip[] musicClips;
 	[SerializeField] private AudioClip[] sfxClips;
-
+	[SerializeField] private AudioClip[] sfxGameOverClips;
+	
 	private bool isPlaying;
 	
 	private void Start(){
-		LevelManager.GameOverEvent += StopLevelAudio;
+		LevelManager.GameOverEvent += GameOver;
 		isPlaying = true;
 		
 		StartPlayingRandomMusic();
+		
 		StartCoroutine(StartRandomSFX());
+	}
+
+	private void GameOver(){
+		StopLevelAudio();
+		
+		int idx = Random.Range(0, sfxGameOverClips.Length);
+		sfxSource.clip = sfxGameOverClips[idx];
+		sfxSource.Play();
 	}
 
 	private void StartPlayingRandomMusic() {
@@ -35,6 +49,7 @@ public class AudioManager : MonoBehaviour
 	private void StopLevelAudio(){
 		isPlaying = false;
 		StartCoroutine(FadeOutThenStop(musicSource));
+		StartCoroutine(FadeOutThenStop(ambienceChatterSource));
 	}
 
 	private IEnumerator FadeOutThenStop(AudioSource audioSource){
