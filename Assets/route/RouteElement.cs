5c90efffa484ba927a663075532f37f34555e0b9
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-07-09 09:57:02 +0200

    Change routes to use perspective camera and Z axis for route distance.

--- Assets/route/RouteElement.cs
+++ Assets/route/RouteElement.cs
@@ -1,45 +1,13 @@
-using System;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 public class RouteElement : MonoBehaviour
 {
-    public float baseSpeed = 1f;
+    public Vector3 direction = Vector3.back;
+    public float speed = 10f;
 
-    public float scaleSpeed;
-    public float scaleVelocity = 0.5f;
-
-    public float moveSpeed;
-
-    public float distanceToPlayer = 10f;
-    public float forwardAngle = 0;
-
-    private void Start()
+    public void Move()
     {
-        scaleSpeed = baseSpeed;
-        moveSpeed = baseSpeed;
-    }
-
-    public void SetBaseSpeed(float value)
-    {
-        baseSpeed = value;
-        scaleSpeed = value;
-        moveSpeed = value;
-    }
-
-    public void ScaleAndMoveElement()
-    {
-        var rot = Quaternion.AngleAxis((forwardAngle - 45), Vector3.forward);
-        var lDirection = rot * new Vector3(1, -1, 0);
-
-        transform.localScale += new Vector3(1, 1, 0) * Time.deltaTime * scaleSpeed;
-        transform.position += lDirection * Time.deltaTime * moveSpeed;
-
-        scaleSpeed += Time.deltaTime * scaleVelocity;
-    }
-
-    public float GetElementScale()
-    {
-        return transform.localScale.x;
+        transform.position += direction * Time.deltaTime * speed;
     }
 }
